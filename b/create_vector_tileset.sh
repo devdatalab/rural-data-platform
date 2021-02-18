045b0c8edca6e22cf0977be217853d4dfa2b834c
Tobias Lunt <toby.lunt@gmail.com>
2021-02-18 18:03:28 -0500

    Update primary tileset build to fix polygon probs
    Tiles were too large, and undersimplified.

--- b/create_vector_tileset.sh
+++ b/create_vector_tileset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this script takes geojson district and shrid data and creates a vector tileset for pushing to mapbox.
-# arguments passed in via Snakefile
+# this requires tippecanoe and tile-join, which are installed in ~/iec/local/share/tippecanoe/
 
 # note: --generate-ids option is required for referencing feature ids in
 # e.g. hover effects. from Mapbox: "mapbox/tippecanoe#615 adds the most
@@ -10,5 +10,14 @@
 # format may change in the future."
 
 # note: --simplification=10: tolerance for line and polygon simplification
-#tippecanoe -o ~/Desktop/map-gl/state_dist.mbtiles --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --extend-zooms-if-still-dropping -z12 --generate-ids ~/Desktop/map-gl/districts.geojson ~/Desktop/map-gl/states.geojson
-tippecanoe -o $1 --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --extend-zooms-if-still-dropping -z12 --generate-ids $2 $3
+#tippecanoe --force -o $1 --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --extend-zooms-if-still-dropping -z9 --generate-ids $2 $3 
+#tippecanoe --force -zg --minimum-zoom=5 -o $1 --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --extend-zooms-if-still-dropping --generate-ids $2 $3 
+
+# create district tileset with zoom range of 5-8
+tippecanoe --force -z9 -Z5 -o $TMP/dist_tile_tmp.mbtiles --read-parallel --simplification=5 --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $IEC/rural_platform/district.geojson
+
+# create shrid tileset with zoom range of 8-10
+tippecanoe --force -z10 -Z9 -o $TMP/shrid_tile_tmp.mbtiles --read-parallel --simplification=20 --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $IEC/rural_platform/shrid.geojson
+
+# merge tilesets
+tile-join --force -o $TMP/rural_portal_data.mbtiles $TMP/dist_tile_tmp.mbtiles $TMP/shrid_tile_tmp.mbtiles
