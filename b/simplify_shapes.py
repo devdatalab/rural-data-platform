ba642dda1d839a049982009c2c949bd26ae3c143
Tobias Lunt <toby.lunt@gmail.com>
2021-02-18 16:49:22 -0500

    Port shapefile simplification over to python
    topojson package

---
+++ b/simplify_shapes.py
@@ -0,0 +1,26 @@
+import topojson as tp
+import geopandas as gpd
+from graphout_py import graphout
+
+# set paths
+dist_in = IEC1 / 'gis/pc11/pc11-district-simplified.shp'
+shrid_in = IEC1 / 'gis/shrug/shrids_corrected.shp'
+dist_out = IEC / 'rural_platform/districts-simplified.shp'
+shrid_out = IEC / 'rural_platform/shrids-simplified.shp'
+
+# read in shapefiles
+dist_shp = gpd.read_file(dist_in)
+shrid_shp = gpd.read_file(shrid_in)
+
+# run the simplifications using topojson package
+dist_simp = tp.Topology(dist_shp, toposimplify=.1)
+shrid_simp = tp.Topology(dist_simp_shp, toposimplify=.1)
+
+# convert back to geodataframe for writing to shp
+# (not too worried about preserving topology as tippecanoe recreates it)
+dist_gdf = dist_simp.to_gdf()
+shrid_gdf = shrid_simp.to_gdf()
+
+# write out new shapefiles
+dist_gdf.to_file(dist_out)
+shrid_gdf.to_file(shrid_out)
