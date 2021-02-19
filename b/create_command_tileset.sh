aa9921ea38de04313429bb7bdca5ecd40a84e430
Tobias Lunt <toby.lunt@gmail.com>
2021-02-19 16:04:18 -0500

    Define tippecanoe full path

--- b/create_command_tileset.sh
+++ b/create_command_tileset.sh
@@ -3,5 +3,5 @@
 # separate tileset of command areas and canal lines
 # this will be used in the scrollytelling map
 
-tippecanoe --force z12 -Z4 -o $TMP/canal-command.mbtiles --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $TMP/canal_lines.geojson $TMP/command_areas.geojson
+~/iec/local/share/tippecanoe/tippecanoe -o $TMP/canal-command.mbtiles --force z12 -Z4 --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $TMP/canal_lines.geojson $TMP/command_areas.geojson
 
