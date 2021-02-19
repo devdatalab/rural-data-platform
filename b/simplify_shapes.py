6489d54f1d20e5535a5c25fafbc3ddab88c54807
Tobias Lunt <toby.lunt@gmail.com>
2021-02-19 16:21:03 -0500

    Comment out all simplification for now

--- b/simplify_shapes.py
+++ b/simplify_shapes.py
@@ -15,20 +15,21 @@ shrid_out = IEC / 'rural_platform/shrids-simplified.shp'
 
 # read in shapefiles
 dist_shp = gpd.read_file(dist_in)
-shrid_shp = gpd.read_file(shrid_in)
+#shrid_shp = gpd.read_file(shrid_in)
 
 # run the simplifications using topojson package
-dist_simp = tp.Topology(dist_shp, toposimplify=.1)
+#dist_simp = tp.Topology(dist_shp, toposimplify=.1)
 # FIXME: hack to fast-track around simplification
 #shrid_simp = tp.Topology(dist_simp_shp, toposimplify=.1)
 
 # convert back to geodataframe for writing to shp
 # (not too worried about preserving topology as tippecanoe recreates it)
-dist_gdf = dist_simp.to_gdf()
-#shrid_gdf = shrid_simp.to_gdf()
+#dist_gdf = dist_simp.to_gdf()
+#oshrid_gdf = shrid_simp.to_gdf()
 
 # write out new shapefiles
-dist_gdf.to_file(dist_out)
+#dist_gdf.to_file(dist_out)
 # FIXME: hack to fast-track around simplification
 #shrid_gdf.to_file(shrid_out)
+dist_shp.to_file(dist_out)
 shrid_shp.to_file(shrid_out)
