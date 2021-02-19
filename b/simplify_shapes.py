c1da93fc65ef7c991a1d3bce6284f947926d9b9a
Tobias Lunt <toby.lunt@gmail.com>
2021-02-19 16:03:27 -0500

    Comment out shrid simplification for now
    Instead rely on tippecanoe --simplification flag
    
    May need to change this later...

--- b/simplify_shapes.py
+++ b/simplify_shapes.py
@@ -1,6 +1,12 @@
 import topojson as tp
 import geopandas as gpd
 
+# paths hack
+from pathlib import Path
+import os
+IEC1 = Path(os.environ.get('IEC1'))
+IEC = Path(os.environ.get('IEC'))
+
 # set paths
 dist_in = IEC1 / 'gis/pc11/pc11-district-simplified.shp'
 shrid_in = IEC1 / 'gis/shrug/shrids_corrected.shp'
@@ -13,13 +19,16 @@ shrid_shp = gpd.read_file(shrid_in)
 
 # run the simplifications using topojson package
 dist_simp = tp.Topology(dist_shp, toposimplify=.1)
-shrid_simp = tp.Topology(dist_simp_shp, toposimplify=.1)
+# FIXME: hack to fast-track around simplification
+#shrid_simp = tp.Topology(dist_simp_shp, toposimplify=.1)
 
 # convert back to geodataframe for writing to shp
 # (not too worried about preserving topology as tippecanoe recreates it)
 dist_gdf = dist_simp.to_gdf()
-shrid_gdf = shrid_simp.to_gdf()
+#shrid_gdf = shrid_simp.to_gdf()
 
 # write out new shapefiles
 dist_gdf.to_file(dist_out)
-shrid_gdf.to_file(shrid_out)
+# FIXME: hack to fast-track around simplification
+#shrid_gdf.to_file(shrid_out)
+shrid_shp.to_file(shrid_out)
